3899ccf332a0766f2ba3df7e4fc52f4859d5f4fc
298A-E9E3 <166412009+298A-E9E3@users.noreply.github.com>
2024-06-24 15:39:20 +0000

    zgfdfgsd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require('express')
+var path = require('path');
+const fs = require('fs');
+function readFile(filePath) {
+    try {
+      const data = fs.readFileSync(filePath);
+      return data.toString();
+    } catch (error) {
+      console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+  }
+const app = express()
+const port = 3000
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.send('<script>window.location.href = window.location.href + "public/index.html";</script>')
+})
 
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
